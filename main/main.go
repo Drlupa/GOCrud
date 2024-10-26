b89baf1f63cc688cc6e031a31c60f0109c110dbf
Bedenashvili David <bedenashvili@mindbox-external.cloud>
2024-10-26 21:02:55 +0300

    changed main.go

--- main/main.go
+++ main/main.go
@@ -4,29 +4,30 @@ import (
 	_ "database/sql"
 	_ "encoding/json"
 	_ "fmt"
-	 "log"
-	 "net/http"
+	"log"
+	"net/http"
 	_ "strconv"
-    _ "github.com/go-sql-driver/mysql"
-    "github.com/gorilla/mux"
+
 	"github.com/Drlupa/GOCrud/crud"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
 )
 
 const (
-	dbDriver  = "mysql"
-	dbUser = "gocrudapi"
-	dbPass = "admin"
-	dbName = "gocrudapi"
+	dbDriver = "mysql"
+	dbUser   = "gocrudapi"
+	dbPass   = "admin"
+	dbName   = "gocrudapi"
 )
 
 func main() {
-	r:= mux.NewRouter()
+	r := mux.NewRouter()
 
 	r.HandleFunc("/user", crud.CreateUserHandler).Methods("POST")
-    // r.HandleFunc("/user/{id}", getUserHandler).Methods("GET")
-    // r.HandleFunc("/user/{id}", updateUserHandler).Methods("PUT")
-    // r.HandleFunc("/user/{id}", deleteUserHandler).Methods("DELETE")
+	r.HandleFunc("/user/{id}", crud.GetUserHandler).Methods("GET")
+	// r.HandleFunc("/user/{id}", updateUserHandler).Methods("PUT")
+	// r.HandleFunc("/user/{id}", deleteUserHandler).Methods("DELETE")
 
 	log.Println("Server listening on :8090")
-    log.Fatal(http.ListenAndServe(":8090", r))
-	}
+	log.Fatal(http.ListenAndServe(":8090", r))
+}
