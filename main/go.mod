d04d2cbc1ee8564bcef43a74e3cdb7a0500e52dd
Bedenashvili David <bedenashvili@mindbox-external.cloud>
2024-10-26 15:29:23 +0300

    added main package

---
+++ main/go.mod
@@ -0,0 +1,3 @@
+module github.com/Drlupa/GOCrud/main
+
+go 1.23.2
---
+++ main/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+    _ "github.com/go-sql-driver/mysql"
+    "github.com/gorilla/mux"
+	"github.com/Drlupa/GOCrud/crud"
+)
+
+const (
+	dbDriver  = "mysql"
+	dbUser = "gocrudapi"
+	dbPass = "admin"
+	dbName = "gocrudapi"
+)
+
+func main() {
+	r:= mux.NewRouter()
+
+	r.HandleFunc("/user", createUserHandler).Methods("POST")
+    // r.HandleFunc("/user/{id}", getUserHandler).Methods("GET")
+    // r.HandleFunc("/user/{id}", updateUserHandler).Methods("PUT")
+    // r.HandleFunc("/user/{id}", deleteUserHandler).Methods("DELETE")
+
+	log.Println("Server listening on :8090")
+    log.Fatal(http.ListenAndServe(":8090", r))
+	}
